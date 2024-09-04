691b488a16adc22bc6ec0459828a9e3871e6574f
Sanjeev Adhikari <sanjeevadhikari75@gmail.com>
2024-09-04 19:57:41 +0545

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path = '/' element = {<h1>Hello world</h1>} />
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
